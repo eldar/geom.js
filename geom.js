e1bd5e53afb330ddcdaad5d9910df393a7c6c534
Eldar Insafutdinov <e.insafutdinov@gmail.com>
2013-12-14 02:04:52 +0100

    can move points

--- geom.js
+++ geom.js
@@ -6,6 +6,11 @@ function copy_vec_2(v) {
     return {x: v.x, y: v.y};
 }
 
+function copy_assign_2(v, w) {
+    v.x = w.x;
+    v.y = w.y;
+}
+
 function sub2(a, b) {
     return vec2(a.x-b.x, a.y-b.y);
 }
@@ -123,19 +128,55 @@ function draw_points(ctx, points, colour) {
 
 //var test_points = JSON.parse('[{"x":384,"y":75},{"x":591,"y":196},{"x":243,"y":454},{"x":203,"y":249},{"x":635,"y":483},{"x":460,"y":298},{"x":711,"y":294},{"x":434,"y":489},{"x":196,"y":330},{"x":228,"y":188},{"x":557,"y":291},{"x":325,"y":298}]');
 
-function init(){
+function init() {
     var canvas = document.getElementById('viewer');
-    var points = [];
-
     var ctx = canvas.getContext('2d');
 
-    canvas.addEventListener('mousedown', function(evt){
-        points.push(getMousePos(canvas, evt));
-        ctx.clearRect(0,0,canvas.width,canvas.height);
+    var points = [];
+    var hull = [];
+    
+    var highlighted = null;
+    var in_drag = false;
 
+    var calculate_frame = function() {
         hull = convexHull(points, ctx);
+    };
+
+    var draw_frame = function() {
+        ctx.clearRect(0,0,canvas.width,canvas.height);
         draw_polygon(ctx, hull, 'green');
         draw_points(ctx, points, 'blue');
         draw_points(ctx, hull, 'red');
+        if(highlighted)
+            draw_point(ctx, highlighted, 3, 'black');
+    };
+
+    canvas.addEventListener('mousemove', function(evt){
+        var pt = getMousePos(canvas, evt);
+        if(in_drag) {
+            copy_assign_2(highlighted, pt);
+            calculate_frame();
+        } else {
+            var eps = 5;
+            highlighted = _.find(points, function(val){
+                return norm2(sub2(pt, val)) < eps;
+            });
+        }
+        draw_frame();
+    });
+
+    canvas.addEventListener('mouseup', function(evt){
+        if(in_drag)
+            in_drag = false;
+    });
+
+    canvas.addEventListener('mousedown', function(evt){
+        if(highlighted) {
+            in_drag = true;
+        } else {
+            points.push(getMousePos(canvas, evt));
+            calculate_frame();
+        }
+        draw_frame();
     });
 }
